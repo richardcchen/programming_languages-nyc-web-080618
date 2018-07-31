256b18f94f51f10aeefb51fcbccdac30db935b72
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 01:15:31 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
+  require "pry"
   # your code here
+  new_hash = {}
+  languages[:oo].each do |lan, data|
+    data.each do |value, type|
+      #binding.pry
+      new_hash[lan] = {value => type, :style => []}
+    end
+    new_hash[lan][:style] << :oo
+  end
+  
+  languages[:functional].each do |lan, data|
+      data.each do |value, type|
+        new_hash[lan] = {value => type, :style => [:functional]}
+      end
+  end
+  new_hash[:javascript][:style] = [:oo, :functional]
+  puts new_hash
+  new_hash
 end
