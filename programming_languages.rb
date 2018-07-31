e9b400eaf89b68f78533e505cf94a2f01e7c661d
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 14:23:54 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages[:oo].each do |lan, data|
+<<<<<<< HEAD
     data.each do |value, type|
       #binding.pry
       new_hash[lan] = {value => type, :style => []}
@@ -20,4 +21,16 @@ def reformat_languages(languages)
   new_hash[:javascript][:style] = [:oo, :functional]
   puts new_hash
   new_hash
+=======
+    new_hash[lan] = {}
+    data.each do |value|
+      #binding.pry
+      new_hash[lan] = {data, :style => [:oo]} 
+    end
+  end
+  languages[:functional].each do |x|
+    
+  end
+  puts new_hash
+>>>>>>> fe4674e7fc9559cf5949d39959fdf1731dc8c8f6
 end
